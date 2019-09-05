45a6cd765b9a8182d2b3910bf509dc076a6313cb
Nilomiranda <nilomiranda3@gmail.com>
2019-09-04 22:35:29 -0300

    Styled components

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import { Container } from '../../Components/Container';
+
 export default function Cart() {
-  return <Text>Hello from Cart</Text>;
+  return (
+    <Container>
+      <Text>Hello from cart</Text>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text, Button } from 'react-native';
 import propTypes from 'prop-types';
 
+import { Container } from '../../Components/Container';
+
 export default function Home({ navigation }) {
   return (
-    <>
+    <Container>
       <Text>Hello from Home</Text>
       <Button title="Go to cart" onPress={() => navigation.navigate('Cart')} />
-    </>
+    </Container>
   );
 }
 
